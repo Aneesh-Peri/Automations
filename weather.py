dd6eafb9858f39e94cffc41d1c1262b4888154bb
Aneesh-Peri <113553361+Aneesh-Peri@users.noreply.github.com>
2023-12-27 22:55:06 -0500

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,6 @@
+import requests
+
+API_KEY = "ea9e7c7022371696e05a1ce0415ac396"
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}"
+new_string = "string"
+
